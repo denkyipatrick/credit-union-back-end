c1da16a01b4cd5a17bf10924ba92f0d86a498326
patrick <official.denkyipatrick@gmail.com>
2022-05-31 15:35:48 +0000

    Change createdAt and updatedAt to use Sequelize.NOW instead of bigint.

--- src/sequelize/migrations/20200809131859-create-transaction.js
+++ src/sequelize/migrations/20200809131859-create-transaction.js
@@ -1,43 +1,43 @@
-'use strict';
+"use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Transactions', {
+    return queryInterface.createTable("Transactions", {
       id: {
         primaryKey: true,
         type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4
+        defaultValue: Sequelize.UUIDV4,
       },
       userAccountId: {
         allowNull: false,
         type: Sequelize.UUID,
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
         references: {
-          model: 'UserAccounts',
-          key: 'id'
-        }
+          model: "UserAccounts",
+          key: "id",
+        },
       },
       type: {
         allowNull: false,
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       amount: {
         allowNull: false,
-        type: Sequelize.DOUBLE
+        type: Sequelize.DOUBLE,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.BIGINT,
-        defaultValue: new Date().getTime()
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.BIGINT,
-        defaultValue: new Date().getTime()
-      }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Transactions');
-  }
+    return queryInterface.dropTable("Transactions");
+  },
 };
--- src/sequelize/models/transaction.js
+++ src/sequelize/models/transaction.js
@@ -1,28 +1,25 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Transaction = sequelize.define('Transaction', {
-    id: { 
-      primaryKey: true, 
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4
+  const Transaction = sequelize.define(
+    "Transaction",
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      userAccountId: { allowNull: false, type: DataTypes.UUID },
+      type: { allowNull: false, type: DataTypes.STRING },
+      amount: { allowNull: false, type: DataTypes.DOUBLE },
     },
-    userAccountId: { allowNull: false, type: DataTypes.UUID },
-    type: { allowNull: false, type: DataTypes.STRING },
-    amount: { allowNull: false, type: DataTypes.DOUBLE },
-    createdAt: {
-      type: DataTypes.BIGINT,
-      defaultValue: new Date().getTime()},
-    updatedAt: {
-      type: DataTypes.BIGINT,
-      defaultValue: new Date().getTime()
-    }
-  }, {});
-  Transaction.associate = function(models) {
+    {}
+  );
+  Transaction.associate = function (models) {
     // associations can be defined here
     Transaction.belongsTo(models.UserAccount, {
-      foreignKey: 'userAccountId',
-      as: 'account'
-    })
+      foreignKey: "userAccountId",
+      as: "account",
+    });
   };
   return Transaction;
 };
